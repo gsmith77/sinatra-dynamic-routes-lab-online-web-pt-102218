be00b18c3c630016f6d7791e71f09508ba5c42d1
grant smith <grantsmith1256@gmail.com>
2019-02-06 20:03:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -19,4 +19,19 @@ class App < Sinatra::Base
     ((@phrase * @num.to_i))
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    @word1 + " " + @word2 + " " + @word3 + " " + @word4 + " " + @word5 + "."
+   end
+
+   get '/:operation/:number1/:number2' do
+     @num1 = params[:number1]
+     @num2 = params[:number2]
+     ((@num1.to_i) + (@num2.to_i)).to_s
+   end
+
 end
